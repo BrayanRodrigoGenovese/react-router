8b307b9ea3af102ad779ffb8c6697f736490508e
Brayan <bittolo.8bit@gmail.com>
2026-02-19 17:41:15 +0100

    creating simple navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,10 @@
+import Navbar from "./components/Navbar";
+
+Navbar;
 export default function App() {
-  return <></>;
+  return (
+    <>
+      <Navbar />
+    </>
+  );
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,3 +1,13 @@
 export default function Navbar() {
-  return <></>;
+  return (
+    <>
+      <nav>
+        <ul>
+          <li>Homepage</li>
+          <li>Chi Siamo</li>
+          <li>Prodotti</li>
+        </ul>
+      </nav>
+    </>
+  );
 }
