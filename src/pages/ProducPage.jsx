52d8b4725aab9d7485dbc1926d44b179067d5da7
Brayan <bittolo.8bit@gmail.com>
2026-02-19 18:26:20 +0100

    connecting to API

--- src/pages/ProducPage.jsx
+++ src/pages/ProducPage.jsx
@@ -1,12 +1,36 @@
+import { useEffect, useState } from "react";
 import Navbar from "../components/Navbar";
+import axios from "axios";
 
 export default function ProductPage() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("https://fakestoreapi.com/products")
+      .then((response) => {
+        setProducts(response.data);
+      })
+      .catch((error) => {
+        console.log("errore API");
+      });
+  }, []);
+
   return (
     <div>
       <Navbar />
-      <h1>Prodotti</h1>
+      <h1>products</h1>
       <div>
-        <p>-- lista di prodotti --</p>
+        {products.map((product) => (
+          <div key={product.id}>
+            <h5>{product.category}</h5>
+            <h2>{product.title}</h2>
+            <p>{product.description}</p>
+            <p>
+              <strong>{product.price} €</strong>
+            </p>
+          </div>
+        ))}
       </div>
     </div>
   );
