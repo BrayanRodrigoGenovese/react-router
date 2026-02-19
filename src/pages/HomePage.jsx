6bd7dc1437ebe0c5037ef1d7e5237190dcc5850b
Brayan <bittolo.8bit@gmail.com>
2026-02-19 17:46:44 +0100

    creating simple homepage

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,3 +1,14 @@
 export default function HomePage() {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <h1>Homepage</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ad non
+        accusantium quas molestiae, neque eius quam consequuntur delectus cumque
+        molestias! Dicta voluptas fugit itaque alias ducimus rerum. Ipsam,
+        harum?
+      </p>
+    </div>
+  );
 }
