89b2c4b355670082c810d011af943f3caeade8aa
Brayan <bittolo.8bit@gmail.com>
2026-02-19 17:44:47 +0100

    creating simple "about page"

--- src/pages/AboutPage.jsx
+++ src/pages/AboutPage.jsx
@@ -1,3 +1,16 @@
+import Navbar from "../components/Navbar";
+
 export default function AboutPage() {
-  return <></>;
+  return (
+    <div>
+      <Navbar />
+      <h1>Chi siamo</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo ad non
+        accusantium quas molestiae, neque eius quam consequuntur delectus cumque
+        molestias! Dicta voluptas fugit itaque alias ducimus rerum. Ipsam,
+        harum?
+      </p>
+    </div>
+  );
 }
